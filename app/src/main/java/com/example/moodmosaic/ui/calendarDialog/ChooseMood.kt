5c3b2984bc52e83af5fdef2f4a59cab74929c41a
Isabel Schönhut <isabel.schoenhut@gmail.com>
2026-07-28 21:32:14 +0200

    refactor: Updated mood selection dialog

--- app/src/main/java/com/example/moodmosaic/ui/calendarDialog/ChooseMood.kt
+++ app/src/main/java/com/example/moodmosaic/ui/calendarDialog/ChooseMood.kt
@@ -1,30 +1,25 @@
 package com.example.moodmosaic.ui.calendarDialog
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import com.example.moodmosaic.db.entities.MoodDefinition
@@ -42,55 +37,66 @@ fun ChooseMood(
         horizontal = innerPadding.calculateStartPadding(LocalLayoutDirection.current),
         vertical = 0.dp,
     )
-    HeadlineMedium(text = "Wie war der heutige Tag?", paddingValues = nPadding)
 
-    LazyVerticalGrid(
-        modifier = Modifier.padding(innerPadding).heightIn(max = 300.dp),
-        columns = GridCells.Adaptive(minSize = 180.dp),
-        contentPadding = PaddingValues(0.dp)
+    HeadlineMedium(
+        text = "Wie war der heutige Tag?",
+        paddingValues = nPadding
+    )
+
+    FlowRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(innerPadding),
+        horizontalArrangement = Arrangement.spacedBy(6.dp),
+        verticalArrangement = Arrangement.spacedBy(0.dp)
     ) {
-        items(moodDefinitions) { mood ->
-            Button(
-                contentPadding = PaddingValues(0.dp),
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(6.dp),
-                colors = ButtonDefaults.filledTonalButtonColors(
+        moodDefinitions.forEach { mood ->
+
+            SuggestionChip(
+                onClick = { onMoodChange(mood.id) },
+
+                border = SuggestionChipDefaults.suggestionChipBorder(
+                    enabled = true,
+                    borderColor =
+                        if (selectedMoodId == mood.id)
+                            mood.colorHex.toColor()
+                        else
+                            MaterialTheme.colorScheme.outlineVariant
+                ),
+
+                colors = SuggestionChipDefaults.suggestionChipColors(
                     containerColor =
                         if (selectedMoodId == mood.id)
-                            MaterialTheme.colorScheme.secondaryContainer
-                        else
-                            Color.Transparent,
-                    contentColor =
-                        Color.Unspecified
-                ),
-                onClick = {
-                    onMoodChange(mood.id)
-                },
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth().padding(start = 4.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Start
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .size(32.dp)
-                            .border(2.dp, MaterialTheme.colorScheme.background, RoundedCornerShape(6.dp))
-                            .background(
-                                color = mood.colorHex.toColor(),
-                                shape = RoundedCornerShape(6.dp)
+                            lerp(
+                                MaterialTheme.colorScheme.surface,
+                                mood.colorHex.toColor(),
+                                0.25f
                             )
-                    )
+                        else
+                            MaterialTheme.colorScheme.surface
+                ),
 
-                    Spacer(Modifier.width(4.dp))
+                label = {
+                    Row(
+                        modifier = Modifier.padding(top = 6.dp, bottom = 6.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(2.dp)
+                    ) {
 
-                    Text(
-                        modifier = Modifier.padding(horizontal = 4.dp),
-                        text = mood.name,
-                        style = MaterialTheme.typography.bodyLarge
-                    )
+                        Box(
+                            modifier = Modifier
+                                .size(18.dp)
+                                .clip(RoundedCornerShape(5.dp))
+                                .background(mood.colorHex.toColor())
+                        )
+
+                        Text(
+                            text = mood.name,
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
                 }
-            }
+            )
         }
     }
 }
